d3e06912188598e4d1a711637d46a4ec2af6241c
aroussel-data <alexandredrr14@gmail.com>
2024-11-18 17:48:46 +0100

    find a way to create threads and have them wait for a join

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+        // create a threads array
+        // fill it with threads by looping over size times and pushing to array
+
+        let mut my_threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            let mut new_thread = thread::spawn(|| {
+                println!("created new thread!");
+            });
+            my_threads.push(new_thread);
+        }
+        return ThreadPool {
+            threads: my_threads,
+        };
     }
     pub fn execute<F>(&self, f: F)
     where
